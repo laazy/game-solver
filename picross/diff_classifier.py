50cee390a6cd666fb60616ae9e047d2679c673d1
Li Hao <yfzm1223@sjtu.edu.cn>
2019-12-18 12:09:44 +0000

    modify ocr

--- picross/diff_classifier.py
+++ picross/diff_classifier.py
@@ -5,7 +5,7 @@ import os
 
 
 class Model:
-    def __init__(self, img_dir: str = "Standard_Number"):
+    def __init__(self, img_dir: str):
         self.imgs = [np.array(Image.open(f"{img_dir}/_{i}.png")) for i in range(10)]
     
     def cal_diff(self, pic):
@@ -21,8 +21,8 @@ class DiffClassifier:
         if model is not None:
             self.load_model(model)
     
-    def build_model(self):
-        self.model = Model(img_dir="Standard_Number")
+    def build_model(self, path):
+        self.model = Model(img_dir=path)
     
     def load_model(self, file):
         with open(file, "rb") as f:
@@ -35,14 +35,21 @@ class DiffClassifier:
         with open(file, "wb") as f:
             pickle.dump(self.model, f)
     
+    def predict_from_path(self, pic_path):
+        target = np.array(Image.open(pic_path))
+        return self.predict(target)
+
     def predict(self, pic):
-        target = np.array(Image.open(pic))
-        res = self.model.cal_diff(target)
+        res = self.model.cal_diff(pic)
         return res.index(min(res))
 
 
 if __name__ == "__main__":
+    # dc = DiffClassifier()
+    # dc.build_model("images/standard_nums")
+    # dc.save_model("diff_model")
+
     file = "Cropped_Number_yfzm/59.png"
     dc = DiffClassifier("diff_model")
-    res = dc.predict(file)
+    res = dc.predict_from_path(file)
     print(res)
