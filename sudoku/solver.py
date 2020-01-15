3da40c522d8bf68b3f58fd56d1569845fa0f4e62
laazy <laaazy@sjtu.edu.cn>
2020-01-15 18:14:24 +0800

    add algo2 for sudoku

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,18 +1,33 @@
 from typing import Tuple, List
 
+# puzzle = [
+#     #       ||       ||
+#     [0, 0, 7, 0, 0, 0, 0, 8, 0],
+#     [4, 0, 5, 0, 0, 3, 0, 0, 0],
+#     [8, 0, 0, 9, 5, 0, 0, 0, 0],
+#     #       ||       ||
+#     [0, 0, 0, 0, 0, 8, 1, 0, 0],
+#     [9, 0, 0, 3, 6, 7, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 4, 0],
+#     #       ||       ||
+#     [7, 0, 0, 0, 0, 9, 2, 0, 0],
+#     [0, 0, 1, 0, 0, 0, 3, 6, 0],
+#     [0, 2, 0, 0, 0, 0, 0, 0, 0]
+# ]
+
 puzzle = [
     #       ||       ||
-    [0, 0, 7, 0, 0, 0, 0, 8, 0],
-    [4, 0, 5, 0, 0, 3, 0, 0, 0],
-    [8, 0, 0, 9, 5, 0, 0, 0, 0],
+    [0, 1, 0, 3, 8, 6, 9, 0, 2],
+    [7, 3, 0, 1, 0, 0, 0, 8, 4],
+    [0, 6, 0, 4, 9, 0, 0, 0, 0],
     #       ||       ||
-    [0, 0, 0, 0, 0, 8, 1, 0, 0],
-    [9, 0, 0, 3, 6, 7, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 4, 0],
+    [8, 5, 6, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 8, 0, 5, 7, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 5, 8],
     #       ||       ||
-    [7, 0, 0, 0, 0, 9, 2, 0, 0],
-    [0, 0, 1, 0, 0, 0, 3, 6, 0],
-    [0, 2, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 7, 8, 0, 2, 0],
+    [6, 9, 0, 0, 0, 4, 0, 3, 7],
+    [1, 0, 7, 2, 6, 3, 0, 9, 0]
 ]
 
 
@@ -44,16 +59,21 @@ class Cell:
     def remove(self, n: int):
         if n in self.hint:
             self.hint.remove(n)
-        self.update()
 
     def __hash__(self):
         return id(self)
 
 
+Cells = List[Cell]
+Matrix = List[Cells]
+
+
 class Solver:
     def __init__(self):
-        self.board: List[List[Cell]] = None
-        pass
+        self.board: Matrix = None
+        self.rows: Matrix = None
+        self.cols: Matrix = None
+        self.tiny: Matrix = None
 
     def solve(self):
         pass
@@ -74,19 +94,21 @@ class Solver:
         self.board = [[
             Cell(j, (ii, jj)) for jj, j in enumerate(i)
         ] for ii, i in enumerate(puzzle)]
+        self.rows = self.board
+        self.cols = list(zip(*self.rows))
+        self.tiny = [[
+            self.board[i // 3 * 3 + j // 3][i % 3 * 3 + j % 3] for j in range(9)]
+            for i in range(9)]
 
     def get_row(self, c: Cell):
-        x, _ = c.pos
-        return [self.board[x][i] for i in range(9)]
+        return self.rows[c.pos[0]]
 
     def get_col(self, c: Cell):
-        _, y = c.pos
-        return [self.board[i][y] for i in range(9)]
+        return self.cols[c.pos[1]]
 
     def get_small(self, c: Cell):
         x, y = c.pos
-        xx, yy = x // 3 * 3, y // 3 * 3
-        return [self.board[i // 3 + xx][i % 3 + yy] for i in range(9)]
+        return self.tiny[x // 3 * 3 + y // 3]
 
     def get_neighbor(self, c: Cell) -> List[Cell]:
         return set().union(self.get_col(c), self.get_row(c), self.get_small(c))
@@ -95,16 +117,28 @@ class Solver:
         if c.num != 0:
             for k in self.get_neighbor(c):
                 k.remove(c.num)
+                k.update()
 
     def set_only(self, cells: List[Cell]) -> None:
-        pass
+        s = {i: list(filter(lambda c: i in c.hint, cells)) for i in range(9)}
+        for k, v in s.items():
+            if len(v) == 1:
+                v[0].num = k
 
     def iter(self):
         for i in self.board:
             for j in i:
                 self.remove_note(j)
+
         for i in range(9):
-            pass
+            self.set_only(self.rows[i])
+            self.set_only(self.cols[i])
+            self.set_only(self.tiny[i])
+
+            
+
+    def check(self):
+        pass
 
     def __str__(self):
         ans = []
