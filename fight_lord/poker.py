a86044381591eee71c1ca202789dbc5252d0a164
laazy <616408874@qq.com>
2022-10-26 21:57:02 +0800

    🔧 refactor poker

--- fight_lord/poker.py
+++ fight_lord/poker.py
@@ -4,20 +4,24 @@ from typing import Dict, Iterable, Tuple, Callable
 
 POKERS_KINDS = '23456789TJQKASB'
 
-CardPlaySet = Iterable[ Tuple[str, str]]
+CardPlaySet = Iterable[Tuple[str, str]]
 SuitFunc = Callable[[str], CardPlaySet]
 CompareFunc = Callable[[str, str], bool]
 
 
 SUIT_FUNC: Dict[str, SuitFunc] = {}
 COMPARE_FUNC: Dict[str, CompareFunc] = {}
+
+
 def suit(func):
     SUIT_FUNC[func.__name__] = func
-    # return func
+    COMPARE_FUNC[func.__name__] = _default_compare
+    return func
+
 
 def compare(func):
-    COMPARE_FUNC[func.__name__] = func
-    # return func
+    COMPARE_FUNC[func.__name__.replace('_compare', '')] = func
+    return func
 
 
 def _pair_n(pokers: str, n) -> CardPlaySet:
@@ -27,29 +31,26 @@ def _pair_n(pokers: str, n) -> CardPlaySet:
             yield p*n, pokers.replace(p, '', n)
 
 
+def _default_compare(p1: str, p2: str) -> bool:
+    return POKERS_KINDS.find(p1) > POKERS_KINDS.find(p2)
+
+
 @suit
 def pair2(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 2)
 
-@compare
-def pair2(p1: str, p2: str) -> bool:
-    return POKERS_KINDS.find(p1) > POKERS_KINDS.find(p2)
 
 @suit
 def pair3(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 3)
 
-@compare
-def pair3(*args): return pair2(*args)
 
 @suit
 def pair4(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 4)
 
-@compare
-def pair4(*args): return pair2(*args)
 
-# @suit
+@suit
 def straight(pokers: str) -> CardPlaySet:
     above_number = 4
     allowing = '3456789TJQK'
@@ -65,22 +66,18 @@ def straight(pokers: str) -> CardPlaySet:
             ans = kinds[begin: end+1]
             yield ans, reduce(lambda x, y: x.replace(y, '', 1), ans, pokers)
 
-@compare
-def straight(*args): return pair2(*args)
-
 
 @suit
 def single(pokers: str) -> CardPlaySet:
     for c in pokers:
         yield c, pokers.replace(c, '', 1)
 
-@compare
-def straight(*args): return pair2(*args)
 
 @suit
 def skip(pokers: str) -> CardPlaySet:
     yield '', pokers
 
+
 @compare
-def skip(p1: str, p2: str)->bool:
+def skip_compare(p1: str, p2: str) -> bool:
     return p2 != ''
