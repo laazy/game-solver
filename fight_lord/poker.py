ff133be38957548705838e598be562418295447d
laazy <616408874@qq.com>
2022-10-26 22:54:36 +0800

    🔧 rename and change DS

--- fight_lord/poker.py
+++ fight_lord/poker.py
@@ -1,31 +1,35 @@
 from collections import Counter
 from functools import reduce, wraps
-from typing import Dict, Iterable, Tuple, Callable
+from typing import Dict, Iterable, List, Tuple, Callable
 
-POKERS_KINDS = '23456789TJQKASB'
+POKERS_KINDS = '3456789TJQKA2SB'
 
 CardPlaySet = Iterable[Tuple[str, str]]
 SuitFunc = Callable[[str], CardPlaySet]
 CompareFunc = Callable[[str, str], bool]
 
 
-SUIT_FUNC: Dict[str, SuitFunc] = {}
+SUIT_FUNC: List[Tuple[str, SuitFunc]] = []
 COMPARE_FUNC: Dict[str, CompareFunc] = {}
 
 
-def suit(func):
-    SUIT_FUNC[func.__name__] = func
+def _suit(func):
+    SUIT_FUNC.append((func.__name__, func))
     COMPARE_FUNC[func.__name__] = _default_compare
     return func
 
 
-def compare(func):
+def _compare(func):
     COMPARE_FUNC[func.__name__.replace('_compare', '')] = func
     return func
 
 
+def is_bigger(name: str, p1: str, p2: str):
+    return COMPARE_FUNC[name](p1, p2)
+
+
 def _pair_n(pokers: str, n) -> CardPlaySet:
-    pokers = ''.join(sorted(pokers))
+    pokers = ''.join(sorted(pokers, key=POKERS_KINDS.find))
     for p, c in Counter(pokers).items():
         if c >= n:
             yield p*n, pokers.replace(p, '', n)
@@ -35,26 +39,26 @@ def _default_compare(p1: str, p2: str) -> bool:
     return POKERS_KINDS.find(p1) > POKERS_KINDS.find(p2)
 
 
-@suit
+@_suit
 def pair2(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 2)
 
 
-@suit
+@_suit
 def pair3(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 3)
 
 
-@suit
+@_suit
 def pair4(pokers: str) -> CardPlaySet:
     yield from _pair_n(pokers, 4)
 
 
-@suit
+@_suit
 def straight(pokers: str) -> CardPlaySet:
     above_number = 4
     allowing = '3456789TJQK'
-    kinds = ''.join(sorted(set(pokers), key=allowing.find))
+    kinds = ''.join(sorted(set(pokers), key=POKERS_KINDS.find))
     if len(kinds) <= above_number:
         return
     for begin in range(len(kinds) - above_number):
@@ -67,17 +71,20 @@ def straight(pokers: str) -> CardPlaySet:
             yield ans, reduce(lambda x, y: x.replace(y, '', 1), ans, pokers)
 
 
-@suit
+@_suit
 def single(pokers: str) -> CardPlaySet:
     for c in pokers:
         yield c, pokers.replace(c, '', 1)
 
 
-@suit
+@_suit
 def skip(pokers: str) -> CardPlaySet:
     yield '', pokers
 
 
-@compare
+@_compare
 def skip_compare(p1: str, p2: str) -> bool:
     return p2 != ''
+
+if __name__ == "__main__":
+    print(list(straight('33456789AAB')))
